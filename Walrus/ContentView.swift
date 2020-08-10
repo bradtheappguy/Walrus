6faf42fa74b88d29fa44f8cd77ef3d782f571d7a
Bradley Anderson <brad@theappguy.guru>
2020-08-10 14:10:39 -0700

    Initial Commit

---
+++ Walrus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Walrus
+//
+//  Created by brad on 8/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
